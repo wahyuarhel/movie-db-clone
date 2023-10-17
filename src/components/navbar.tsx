4d15b6442afd060cb3496a1dd30eb4cb49bf653a
wahyuarhel <akunwahyuarhel@gmail.com>
2023-10-17 13:48:27 +0700

    try to using accordion for submenu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,17 @@
 "use client"
+import LoginModal from '@/app/components/loginModal';
+import { Accordion, AccordionItem, Navbar, NavbarBrand, NavbarContent, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from '@nextui-org/react';
 import Link from 'next/dist/client/link';
-import { useState } from 'react';
-import { FaPlus } from 'react-icons/fa';
-import { GiHamburgerMenu } from 'react-icons/gi'
-import { BiSearch } from 'react-icons/bi'
-function Navbar() {
-  const [menuIndex, setMenuIndex] = useState<number>()
+import { usePathname } from 'next/navigation';
+import React, { Key, useEffect, useState } from 'react';
+import { BiSearch } from 'react-icons/bi';
+function AppNavbar() {
+  const currentPage = usePathname()
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false)
+  const [selectedMenuKeys, setSelectedMenuKeys] = React.useState<Iterable<Key>>(new Set(['']));
+
+
   const menuItems = [
     {
       title: 'Movie',
@@ -20,9 +26,9 @@ function Navbar() {
       title: 'Tv Show',
       subMenu: [
         { label: 'Popular', path: '/tv' },
-        { label: 'Airing Today', path: '/' },
-        { label: 'On Tv', path: '/' },
-        { label: 'Top Rated', path: '/' }
+        { label: 'Airing Today', path: '/airing-today' },
+        { label: 'On Tv', path: '/on-tv' },
+        { label: 'Top Rated', path: '/top-rated' }
       ]
     },
     {
@@ -33,78 +39,111 @@ function Navbar() {
     },
   ]
 
-  function MenuItems() {
-    return menuItems.map((e, i) =>
-      <div key={i}
-        className='relative cursor-pointer py-3 px-2'
-        onMouseEnter={() => onHoverAtMenu(i)}
-        onMouseLeave={onMouseLeave}
-      >
-        <p className='text-white cursor-pointer'>{e.title}</p>
-        {menuIndex == i
-          ?
-          <Submenu key={i} menu={e.subMenu.map((e) => e)} openDropdown={menuIndex == i} className='translate-y-[5px]' />
-          : null
-        }
-      </div>
-    )
+  function handleLoginModal() {
+    setIsOpenModal((prev) => !prev)
+    console.log('handleLoginModal triggered')
   }
+
+  function handleMenuSelectedStyle(pathName: string) {
+    if (pathName === currentPage) {
+      return 'text-white/100 underline'
+    }
+    return ''
+  }
+  const handleOpenMenu = () => setIsMenuOpen(prev => !prev)
+  const handleCloseMenu = () => {
+    setIsMenuOpen(false)
+  }
+
 
   return (
-    <div className='bg-[#032541] fixed top-0  w-[100%] h-[64px] flex z-[100]'>
-      {/** Navbar for desktop */}
-      <div className='hidden md:flex container m-auto gap-5 justify-between items-center flex-grow '>
-        <div className='flex flex-row gap-3 items-center'>
-          <Link href={'/'}>
-            <div className='flex flex-row items-center gap-3 '>
-              <p className='font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-lightGreen to-lightBlue'>MovieDB</p>
-              <div className='w-[50px] h-[20px] rounded-full bg-lightBlue'></div>
+    <>
+      <LoginModal
+        isOpen={isOpenModal}
+        onOpenChange={handleLoginModal}
+      />
+      <Navbar
+        isMenuOpen={isMenuOpen}
+        onMenuOpenChange={setIsMenuOpen}
+        classNames={{
+          wrapper: 'max-w-[1536px]'
+        }}
+      >
+        <NavbarMenu className='md:hidden'>
+          <NavbarMenuItem className='text-white'>
+            <Accordion
+              isCompact
+              selectedKeys={selectedMenuKeys}
+              onSelectionChange={setSelectedMenuKeys}
+              className='px-0 py-0'
+              showDivider={false}
+            >
+              {menuItems.map((menu, i) =>
+                <AccordionItem key={i}
+                  aria-label={menu.title}
+                  title={menu.title}
+                  indicator={<p></p>}
+                  classNames={{
+                    title: 'text-white text-xl font-semibold',
+                    trigger: 'py-0 pb-2',
+                    content: 'pl-2 py-0 pb-2'
+                  }}
+                >
+                  <ul>
+                    {menu.subMenu.map((subMenu, i) =>
+                      <li key={i}>
+                        <Link href={subMenu.path}
+                          className={`text-base text-white/70 ${handleMenuSelectedStyle(subMenu.path)}`}
+                          onClick={handleCloseMenu}
+                        >
+                          {subMenu.label}
+                        </Link>
+                      </li>
+                    )}
+                  </ul>
+
+                </AccordionItem>
+              )}
+            </Accordion>
+            <div
+              className='cursor-pointer '
+              onClick={handleLoginModal}>
+              Login
             </div>
-          </Link>
-          <MenuItems />
-        </div>
-        <div><FaPlus className='text-white' /></div>
-      </div>
-      {/** Navbar for mobile */}
-      <div className='flex justify-between md:hidden items-center flex-1 min-w-[320px]'>
-        <GiHamburgerMenu className='text-white mx-3' />
-        <Link href={'/'}>
-          <div className='flex gap-3 items-center cursor-pointer'>
-            <p className='font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-lightGreen to-lightBlue'>MovieDB</p>
-            <div className='w-[50px] h-[20px] rounded-full bg-lightBlue'></div>
-          </div>
-        </Link>
-        <BiSearch className='text-white mx-3' />
-      </div>
-    </div>
+          </NavbarMenuItem>
+        </NavbarMenu>
+        <NavbarContent >
+          <NavbarMenuToggle
+            onClick={handleOpenMenu}
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            className='md:hidden text-white'
+          />
+          <NavbarBrand className='hidden md:block'>
+            <Link href={'/'} >
+              <div className='flex flex-row items-center gap-3 ' >
+                <p className='font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-lightGreen to-lightBlue'>MovieDB</p>
+                <div className='w-[50px] h-[20px] rounded-full bg-lightBlue'></div>
+              </div>
+            </Link>
+          </NavbarBrand>
+        </NavbarContent>
+        <NavbarContent>
+          <NavbarBrand className='md:hidden'>
+            <Link href={'/'} onClick={handleCloseMenu}>
+              <div className='flex flex-row items-center gap-3 '>
+                <p className='font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-lightGreen to-lightBlue'>MovieDB</p>
+                <div className='w-[50px] h-[20px] rounded-full bg-lightBlue'></div>
+              </div>
+            </Link>
+          </NavbarBrand>
+        </NavbarContent>
+        <NavbarContent justify='end'>
+          <BiSearch className='text-white mx-3' />
+        </NavbarContent>
+      </Navbar>
+    </>
   );
-
-  function onHoverAtMenu(index: number) {
-    setMenuIndex(index)
-
-  }
-
-  function onMouseLeave() {
-    setMenuIndex(undefined)
-  }
-
-  interface SubmenuPropTypes<T> {
-    label: T[]
-
-  }
-
-  function Submenu({ menu, className, openDropdown = false, }: { menu: { label: string, path: string }[], className?: string, openDropdown: boolean, }) {
-    return (
-      <div className={`bg-white rounded-lg py-2 min-w-[200px] shadow-lg absolute ${className}`} onMouseEnter={() => openDropdown = true}>
-        {menu.map((e, i) =>
-          <Link href={e.path} key={i}>
-            <p key={i} className='px-5 py-1 hover:bg-blue-300 cursor-pointer '>{e.label}</p>
-          </Link>
-        )}
-      </div>
-    )
-  }
 
 }
 
-export default Navbar
+export default AppNavbar
